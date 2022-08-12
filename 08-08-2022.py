390254cab48e80ebdbc6399c669a5ce744129a7b
hungle2711 <hungubbc@gmail.com>
2022-08-12 21:36:22 +0700

    a

--- 08-08-2022.py
+++ 08-08-2022.py
@@ -1,6 +1,36 @@
-def sum(a, b):
-    '''
-    Hàm này là hàm de tinh tong hai so.
-    '''
-    pass
-print(sum.__doc__)
+def add_item(myTempList, item):    
+    new_myTemplist = myTempList.append(item)
+
+def find_index_item(myTempList, item_name):
+    result = -1
+    length = len(myTempList)
+    for i in range(length):
+        if myTempList[i]['name'] == item_name:
+            result = i
+    return result
+
+def remove_item(myTempList, item_name):
+    if find_index_item(myTempList, item_name) > -1:
+        del myTempList[find_index_item(myTempList, item_name)]
+    else:
+        print(item_name + " not in list")
+
+expenses = input('Tell me your expenses?  ' )
+print("Your expenses:", expenses)
+        
+print("What do you want to do? -\n"\
+        "1. Add\n" \
+        "2. Remove")
+option = int(input("Select option 1 or 2: "))
+name_input = input("Item name: ")
+if option == 1:
+    cost_input = int(input("Item cost: "))
+    date_input = input("Date: ")
+    item = {'name': name_input, 'cost':cost_input, 'date':date_input}
+    add_item(expenses, item)
+    print("Your expenses: ", expenses)
+elif option == 2:
+    remove_item(expenses, name_input)
+    print("Your expenses: ", expenses)
+else:
+    print("Invalid input")
