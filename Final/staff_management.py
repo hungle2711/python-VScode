2b3cc17ded1d2bb013d0375509038ea6c58d3a19
hungle2711 <hungubbc@gmail.com>
2022-09-21 21:34:46 +0700

    Final

--- Final/staff_management.py
+++ Final/staff_management.py
@@ -28,31 +28,7 @@ def check_blank_data():
         return True
     else:
         msb.showwarning('Warnning',message+'is blank!')
-        return False
-
-def validate_phone(value):
-    # Bắt lỗi nhập số điện thoại
-    # '^0': ký tự đầu phải là 0; 'd{9}': yêu cầu phải là số 
-    pattern = '^0\d{9}'
-    if entry_value_phone.get() !='':
-        if re.fullmatch(pattern, entry_value_phone.get()) is None:
-            return False
-        else:
-            return True
-    
-def validate_age(value):
-    pattern = '\d+'
-    if entry_value_age.get() !='':
-        if re.fullmatch(pattern, entry_value_age.get()) is None:
-            return False
-        else:
-            return True
-
-def on_invalid_phone():
-    msb.showwarning('Warnning','Invalid phone number!\nIt should be like 0xxxxxxxxx') 
-        
-def on_invalid_age():
-    msb.showwarning('Warnning','Invalid Age!\nIt should be number which is greater than zero')   
+        return False  
         
 def save_data():
     wb.save('staff_list.xlsx')
@@ -68,7 +44,6 @@ def clear_data():
 def item_selected():
     selected_item = tvw_info.selection()
     item = tvw_info.item(selected_item,option='values')
-#     item = items['values']
     return item
 
 def view_staff():
@@ -101,14 +76,10 @@ def view_all_staff():
 def add_staff():
     new_id=entry_value_id.get()    
     new_age = ''
-    if validate_age == False:
-        entry_value_age.set('')
     new_age=entry_value_age.get()    
     new_full_name=entry_value_full_name.get()    
     new_email=entry_value_email.get()    
     new_phone = ''
-    if validate_phone == False:        
-        entry_value_phone.set('')
     new_phone=entry_value_phone.get()    
     new_deparment=cbx_selected_department.get()    
     new_staff=[new_id,new_full_name,new_email,new_age,new_phone,new_deparment]
@@ -119,14 +90,14 @@ def add_staff():
             if row[0].row > 1:
                 if row[0].value == new_id:
                     print(row[0].value)
-                    msb.showwarning('Warnning','ID is existed!')
+                    msb.showwarning('Warnning','MÃ ĐÃ TỒN TẠI!')
                     return 
         # Thực hiện thêm vào file excel nếu không trùng
         last_row = ws.max_row
         last_column = 6
         for col in range(1,last_column+1):
             ws.cell(row=last_row+1,column=col).value=new_staff[col-1]
-        msb.showinfo('Anoucement','Add successful!')
+        msb.showinfo('Anoucement','THÊM THÀNH CÔNG!')
         save_data()
         clear_data()
         view_all_staff()
@@ -137,14 +108,10 @@ def update_staff():
     
     update_id = entry_value_id.get()
     update_age = ''
-    if validate_age == False:
-        entry_value_age.set('')
     update_age=entry_value_age.get()    
     update_full_name=entry_value_full_name.get()    
     update_email=entry_value_email.get()    
     update_phone = ''
-    if validate_phone == False:        
-        entry_value_phone.set('')
     update_phone=entry_value_phone.get()    
     update_department=cbx_selected_department.get()    
     update_staff=[update_id,update_full_name,update_email,update_age,update_phone,update_department]
@@ -154,7 +121,7 @@ def update_staff():
             for col in range(0,ws.max_column):
                 row[col].value=update_staff[col]
                 
-    msb.showinfo('Anoucement','Update successful!')
+    msb.showinfo('Anoucement','CẬP NHẬT THÀNH CÔNG!')
     save_data()
     clear_data()        
     wb = load_workbook('staff_list.xlsx')
@@ -163,86 +130,22 @@ def update_staff():
     
 def delete_staff():
     item = item_selected()
-    answer = msb.askyesno('Confirm','Do you want to delete?')
+    answer = msb.askyesno('Confirm','BẠN CÓ MUỐN XÓA KHÔNG?')
     if answer == 1:
         for row in ws:
             if row[0].value == item[0]:
                 ws.delete_rows(row[0].row) 
                 save_data()
-                msb.showinfo('Anoucement','Delete successful!')        
+                msb.showinfo('Anoucement','XÓA THÀNH CÔNG!')        
     view_all_staff()
-        
-def delete_all_staff():
-    answer = msb.askyesno('Confirm','Do you want to delete?')
-    if answer == 1:
-        max_row = ws.max_row+1
-        for row in range(2,max_row):
-            ws.delete_rows(2)
-        save_data()
-        msb.showinfo('Anoucement','Delete successful!')  
-    
-    view_all_staff()
-
-def search():
-    tvw_value = []
-    index = -1
-    for item in tvw_info.get_children():
-                tvw_info.delete(item)
-    if cbx_selected_search.get() == 'ID':
-        index = 0
-    if cbx_selected_search.get() == 'Full Name':
-        index = 1
-    if cbx_selected_search.get() == 'Email':
-        index = 2
-    if cbx_selected_search.get() == 'Age':
-        index = 3
-    if cbx_selected_search.get() == 'Phone':
-        index = 4
-    if cbx_selected_search.get() == 'Department':
-        index = 5
-    for row in ws:
-        if row[index].value == entry_value_search.get():
-            for col in range(1,ws.max_column+1):
-                tvw_value.append(ws.cell(row[index].row,column=col).value)
-            tvw_info.insert('',tk.END,value=tvw_value)
-
-def sort():
-    sort_list = []    
-    index = -1
-    for item in tvw_info.get_children():
-        tvw_info.delete(item)
-    if cbx_selected_sort.get() == 'ID':
-        index = 0
-    if cbx_selected_sort.get() == 'Full Name':
-        index = 1
-    if cbx_selected_sort.get() == 'Email':
-        index = 2
-    if cbx_selected_sort.get() == 'Age':
-        index = 3
-    if cbx_selected_sort.get() == 'Phone':
-        index = 4
-    if cbx_selected_sort.get() == 'Department':
-        index = 5
-    for row in ws:
-        sort_list.append(row[index].value)
-    sort_list.pop(0)
-    sort_list.sort()
-    for item in sort_list:
-        for r in range(2,ws.max_row+1):
-            tvw_value = []     
-            cell_value = ws.cell(row=r,column=index+1).value
-            if cell_value == item:
-                for c in range(1,ws.max_column+1):
-                    tvw_value.append(ws.cell(row=r,column=c).value)
-                tvw_info.insert('',tk.END,value=tvw_value)
 
 def exit():
     window.destroy()
 
 # Khởi tạo window chính
 window = tk.Tk()
-window.title('MSD STAFF MANAGEMENT')
-window.geometry('465x600')
+window.title('STAFF MANAGEMENT')
+window.geometry('950x450')
 window.resizable(False,False)
 
 # Khởi tạo đối tượng đọc file excel
@@ -255,14 +158,15 @@ entry_value_full_name = tk.StringVar()
 entry_value_email = tk.StringVar()
 entry_value_age = tk.StringVar()
 entry_value_phone = tk.StringVar()
-entry_value_search = tk.StringVar()
-cbx_selected_search = tk.StringVar()
 cbx_selected_department = tk.StringVar()
-cbx_selected_sort = tk.StringVar()
 info_column = ('id','full_name','email','age','phone','department')
 
+# Khởi tạo frame
+frame1 = ttk.Frame(window)
+
+
 # Khởi tạo các trường để nhập thông tin
-frame_input_info = ttk.Labelframe(window,text='Input information')
+frame_input_info = ttk.Labelframe(frame1,text='Nhập thông tin')
 
 label_id = ttk.Label(frame_input_info,text='ID')
 label_id.grid(row=0,column=0,sticky='e',padx='5',pady='5')
@@ -286,9 +190,6 @@ entry_id = ttk.Entry(frame_input_info,textvariable=entry_value_id)
 entry_id.grid(row=0,column=1,sticky='w',pady='5')
 
 entry_age = ttk.Entry(frame_input_info,textvariable=entry_value_age)
-vcmd_age = (entry_age.register(validate_age),'%P')
-ivcmd_age = (entry_age.register(on_invalid_age))
-entry_age.config(validate='focusout', validatecommand=vcmd_age, invalidcommand=ivcmd_age)
 entry_age.grid(row=0,column=3,sticky='w',pady='5')
 
 entry_full_name = ttk.Entry(frame_input_info,textvariable=entry_value_full_name)
@@ -298,9 +199,6 @@ entry_email = ttk.Entry(frame_input_info,textvariable=entry_value_email)
 entry_email.grid(row=1,column=3,sticky='w',pady='5')
 
 entry_phone = ttk.Entry(frame_input_info,textvariable=entry_value_phone)
-vcmd_phone = (entry_phone.register(validate_phone),'%P')
-ivcmd_phone = (entry_phone.register(on_invalid_phone))
-entry_phone.config(validate='focusout', validatecommand=vcmd_phone, invalidcommand=ivcmd_phone)
 entry_phone.grid(row=2,column=1,sticky='w',pady='5')
 
 cbx_department = ttk.Combobox(frame_input_info,state= "readonly",textvariable=cbx_selected_department)
@@ -309,7 +207,7 @@ cbx_department.current(0)
 cbx_department.grid(row=2,column=3,sticky='w',pady='5')
 
 # Khởi tạo các Button để thực hiện các chức năng
-frame_function = ttk.Labelframe(window,text='Function')
+frame_function = ttk.Labelframe(frame1,text='Chức năng')
 
 btn_view = ttk.Button(frame_function, text='View All', command=view_all_staff)
 btn_view.grid(row=0,column=0,sticky='w',padx='5',pady='5')
@@ -323,9 +221,6 @@ btn_update.grid(row=0,column=2,sticky='w',padx='5',pady='5')
 btn_delete = ttk.Button(frame_function, text='Delete', command=delete_staff)
 btn_delete.grid(row=0,column=3,sticky='w',padx='5',pady='5')
 
-btn_delete_all_btn = ttk.Button(frame_function, text='Delete All', command=delete_all_staff)
-btn_delete_all_btn.grid(row=0,column=4,sticky='w',padx='5',pady='5')
-
 btn_get_info = ttk.Button(frame_function, text='Get Info', command=view_staff)
 btn_get_info.grid(row=1,column=0,sticky='w',padx='5',pady='5')
 
@@ -333,32 +228,10 @@ btn_clear = ttk.Button(frame_function, text='Clear', command=clear_data)
 btn_clear.grid(row=1,column=1,sticky='w',padx='5',pady='5')
 
 btn_exit = ttk.Button(frame_function, text='Exit', command=exit)
-btn_exit.grid(row=1,column=2,sticky='w',padx='5',pady='5')
+btn_exit.grid(row=0,column=4,sticky='w',padx='5',pady='5')
 
-# Khởi tạo các trường để search
-frame_search = ttk.Frame(window)
-
-entry_search = ttk.Entry(frame_search, textvariable=entry_value_search)
-entry_search.grid(row=0,column=0,sticky='w',padx='5',pady='5')
-
-cbx_search = ttk.Combobox(frame_search,state= "readonly",textvariable=cbx_selected_search, width=13)
-cbx_search['value']=('-- Search by --','ID','Full Name','Email','Age','Phone','Department')
-cbx_search.current(0)
-cbx_search.grid(row=0,column=1,sticky='e',pady='5')
-
-btn_search = ttk.Button(frame_search,text='Search',command=search)
-btn_search.grid(row=0,column=2,sticky='w',padx='5',pady='5')
-
-cbx_sort = ttk.Combobox(frame_search,state= "readonly",textvariable=cbx_selected_sort, width=10)
-cbx_sort['value']=('-- Sort by --','ID','Full Name','Email','Age','Phone','Department')
-cbx_sort.current(0)
-cbx_sort.grid(row=1,column=1,sticky='e',pady='5')
-
-btn_sort = ttk.Button(frame_search,text='Sort',command=sort)
-btn_sort.grid(row=1,column=2,sticky='e',padx='5',pady='5')
-
-# Khởi tạo các trường để hiển thị thông tin
-frame_display_info = ttk.Labelframe(window, text='Display Information')
+# Frame hiển thị thông tin nhân viên
+frame_display_info = ttk.Labelframe(window, text='Thông tin nhân viên')
 
 tvw_info = ttk.Treeview(frame_display_info,column=info_column,show='headings')
 tvw_info.column('id',width=30)
@@ -378,14 +251,13 @@ tvw_info.heading('department',text='Department')
 tvw_info.grid(row=1,column=0,sticky='w',padx='5',pady='5')
 tvw_info.bind('<<TreeviewSelect>>',item_selected())
 
-scrollbar = ttk.Scrollbar(frame_display_info,orient='vertical',command=tvw_info.yview)
-tvw_info['yscrollcommand']=scrollbar.set
-scrollbar.grid(row=1,column=1,sticky='w',padx='5',pady='5')
 
 # Sắp xếp các frame
-frame_input_info.pack(padx='10',pady='20',anchor='w')
-frame_function.pack(padx='10',pady='20',anchor='w')
-frame_search.pack(padx='10',pady='0',anchor='w')
-frame_display_info.pack(padx='10',pady='0',expand=True,fill='y',anchor='w')
+frame_input_info.pack(padx='10',pady='20', anchor='w')
+frame_function.pack(padx='10',pady='20', anchor='w')
+frame1.pack(padx='10',pady='20', anchor='w',side='left')
+frame_display_info.pack(padx='10',pady='100', anchor='w',side='right')
+
+
 
 window.mainloop()
